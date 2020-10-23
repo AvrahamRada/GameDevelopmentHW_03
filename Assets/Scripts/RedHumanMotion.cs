40e189e4f6b3c9cbb4820739851941b7352844ab
Avraham Rada <avraham1992rada@gmail.com>
2020-10-23 14:55:08 +0300

    Update RedHumanMotion.cs

--- Assets/Scripts/RedHumanMotion.cs
+++ Assets/Scripts/RedHumanMotion.cs
@@ -26,9 +26,16 @@ public class RedHumanMotion : MonoBehaviour
             vMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
             GetComponent<Animation>().Play("ShieldWarrior@Walk01");
             transform.Rotate(0, hMove, 0);
+
+            // pos is the x-z coordinate of a character
+            Vector3 pos = new Vector3(transform.position.x, 0, transform.position.z);
+            Vector3 point;
+            point.y = Terrain.activeTerrain.SampleHeight(pos) - transform.position.y; // delta in Y direction
+            Vector3 direction = Vector3.forward * vMove;
+            direction.y = point.y;
             //transform.Translate(Vector3.forward * vMove);
             //TransformDirection - transform cordinates to globals
-            cController.Move(transform.TransformDirection(Vector3.forward * vMove));
+            cController.Move(transform.TransformDirection(direction));
         }
         else
         {
